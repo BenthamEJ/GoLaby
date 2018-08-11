9aacc06592fa31d72e50d27bb6edef351a21af3f
BenthamEJ <benthamej@gmail.com>
2018-08-12 00:41:41 +0100

    introducing Gaussian elimination normalisation function

--- console/console.go
+++ console/console.go
@@ -1,5 +1,7 @@
 package main
 
 func main() {
-	print("Checking console works this way.")
+	print("Checking console works this way. Wait, why is this text red in the output?\n" +
+		"If it was wrong, it should not output this text at all.\n" +
+		"Maybe it's just something about GoLand.\n")
 }
---
+++ maths/linear/gauss.go
@@ -0,0 +1,24 @@
+package linear
+
+import "math"
+
+func Normal(a [100][100]float64, b [100]float64, m int32) {
+	var i, j int32
+	var big float64
+
+	for i = 0; i < m; i++ {
+		big = 0.0
+
+		for j = 0; j < m; j++ {
+			if big < math.Abs(a[i][j]) {
+				big = math.Abs(a[i][j])
+			}
+		}
+
+		for j = 0; j < m; j++ {
+			a[i][j] = a[i][j] / big
+		}
+
+		b[i] = b[i] / big
+	}
+}
