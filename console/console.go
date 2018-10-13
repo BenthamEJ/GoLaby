d3fa7e3f518edcdc812adef32b46d2d501da5da9
BenthamEJ <benthamej@gmail.com>
2018-10-13 21:29:51 +0100

    big maths attempt before discovering math/big

--- console/console.go
+++ console/console.go
@@ -1,14 +1,7 @@
 package main
 
-import (
-	"github.com/BenthamEJ/goLaby/physics"
-	"github.com/BenthamEJ/goLaby/physics/cosmic"
-)
-
 func main() {
-	print("Console works without being called main.\n\n")
+	//print("Console works without being called main.\n\n")
 
-	println("Question: What is the Hawking Radiation Temperature of a 1 solar mass black hole?")
-	answer := cosmic.TempH(physics.SolarMass())
-	println("Answer:", answer)
+	//insert code here
 }
